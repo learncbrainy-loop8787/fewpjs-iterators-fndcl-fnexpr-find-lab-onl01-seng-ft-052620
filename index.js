8013562ca0d95091a1606330bf51f556dce7aee8
learncbrainy-loop8787 <koreykendrick1@gmail.com>
2021-01-26 18:05:05 -0600

    commit tree 24b72e826fb5e310a32a79a82204f06e45fc4173

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-superbowlWin = (record) => {
-  let result = record.find( record => record.result === "W" )
-  return !!result ? result.year : undefined
+const testVar = {}
+
+function testFunc() {
+  return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+
+function superbowlWin(arrayOfObjects) {
+  const winner = arrayOfObjects.find( function(object) { return object.result === "W" })
+    if (winner) {
+      return winner.year
+    }
+}
+
+superbowlWin(record)
